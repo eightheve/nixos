99213acb00d93ab2a343d3a5c4dde568db101622
doppelsana <sana@doppel.moe>
2026-01-15 08:42:49 -0500

    lock pin nvf to working version of nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,8 +5,9 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nvf-nixpkgs.url = "github:NixOS/nixpkgs/c0b0e0fddf73fd517c3471e546c0df87a42d53f4";
     nvf.url = "github:notashelf/nvf";
-    nvf.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.inputs.nixpkgs.follows = "nvf-nixpkgs";
     nixcord.url = "github:kaylorben/nixcord";
     sana-website = {
       url = "github:eightheve/sana-website";

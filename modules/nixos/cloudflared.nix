8730ecd98de647845f589cf939c3b6322ae8a2e1
doppelsana <sana@doppel.moe>
2026-01-19 04:52:46 -0500

    adding cloudflared module

--- modules/nixos/cloudflared.nix
+++ modules/nixos/cloudflared.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.myModules.cloudflared;
@@ -10,21 +11,26 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    services.cloudflared = {
-      enable = true;
-      tunnels = {
-        "doppelsana" = {
-          default = "http_status:404";
-          ingress = {
-            "doppel.moe" = "http://localhost:443";
-            "*.doppel.moe" = "http://localhost:443";
-          };
-        };
-      };
-    };
+    environment.etc."cloudflared/config.yml".text = ''
+      tunnel: a047f32f-c101-4f7c-9986-54c01a0eaaf2
+      credentials-file: /etc/cloudflared/token.env
 
-    systemd.servies.cloudflared-tunnel-doppelsana.serviceConfig = {
-      EnvironmentFile = "/etc/cloudflared/token.env";
+      ingress:
+        - service: https://localhost:443
+          originRequest:
+            noTLSVerify: true
+            httpHostHeader: doppel.moe
+    '';
+
+    systemd.services.cloudflared = {
+      wantedBy = ["multi-user.target"];
+      after = ["network-online.target"];
+      serviceConfig = {
+        ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate --config /etc/cloudflared/config.yml run";
+        Restart = "always";
+        EnvironmentFile = "/etc/cloudflared/token.env";
+        DynamicUser = true;
+      };
     };
   };
 }

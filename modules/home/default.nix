371a75f5ae1623b71b354ed2c0aed2846cc32db2
doppelsana <sana@doppel.moe>
2026-01-29 11:20:05 -0500

    suckless restructure
    moving existing dwm/slstatus configurations into their own subdirs in modules/home/suckless, should make compartmentalization easier. Also attempting to make dwm's config.h declared by nix, instead of overriding text in a standalone config.h

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,12 +3,12 @@
     ./beets.nix
     ./colorscheme.nix
     ./discord.nix
-    ./dwm
     ./hyprland
     ./fastfetch.nix
     ./fish.nix
     ./kitty.nix
     ./nvim.nix
+    ./suckless
     ./supersonic.nix
     ./vintagestory.nix
     ./xdg.nix

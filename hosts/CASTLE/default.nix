0a1f20077bb7a641172212e499b89e58d6b36275
doppelsana <sana@doppel.moe>
2026-02-03 05:27:35 -0500

    adding CASTLE

---
+++ hosts/CASTLE/default.nix
@@ -0,0 +1,42 @@
+{...}: {
+  imports = [
+    ./hardware.nix
+  ];
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+  };
+
+  hardware = {
+    bluetooth.enable = true;
+    sensor.iio.enable = true;
+  };
+
+  services.xserver = {
+    enable = true;
+    displayManager.startx.enable = true;
+    videoDrivers = ["modesetting"];
+  };
+
+  myModules.networking = {
+    enable = true;
+    hostName = "CASTLE";
+  };
+
+  myUsers.sana = {
+    enable = true;
+    useHomeManager = true;
+    windowManager = "dwm";
+  };
+
+  networking.firewall.enable = false;
+
+  system.stateVersion = "25.11";
+}

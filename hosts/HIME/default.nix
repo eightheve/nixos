a2d649c40c660e2f43a75a92823a182bcea0c214
doppelsana <sana@doppel.moe>
2026-02-04 12:53:32 -0500

    getting HIME set up for raid array

--- hosts/HIME/default.nix
+++ hosts/HIME/default.nix
@@ -8,6 +8,20 @@
     device = "/dev/disk/by-id/wwn-0x55cd2e404b4d7cc5";
   };
 
+  boot.swraid = {
+    enable = true;
+    mdadmConf = ''
+      MAILADDR root
+      DEVICE /dev/disk/by-id/scsi-35000c*
+      ARRAY /dev/md0 level=5 num-devices=12 UUID=86d3d56d:a781c339:a7c90d5d:b252403a;
+    '';
+  };
+
+  fileSystems."/srv/data" = {
+    device = "/dev/md0";
+    fsType = "ext4";
+  };
+
   myModules.networking = {
     enable = true;
     hostName = "HIME";

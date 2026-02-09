1a9af9bf9be86c007ee1401a1237ed9aaaa754da
doppelsana <sana@doppel.moe>
2026-02-09 14:23:25 -0500

    adding GARDEN (x1 yoga)
    this will probably be renamed in the future and may or may not replace SATELLITE

---
+++ hosts/GARDEN/default.nix
@@ -0,0 +1,53 @@
+{...}: {
+  imports = [
+    ./hardware.nix
+  ];
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+  };
+
+  boot.initrd.kernelModules = ["ideapad_laptop"];
+  hardware = {
+    bluetooth.enable = true;
+    sensor.iio.enable = true;
+    trackpoint = {
+      enable = true;
+    };
+  };
+
+  services.xserver = {
+    enable = true;
+    displayManager.startx.enable = true;
+    videoDrivers = ["modesetting"];
+  };
+
+  myModules.networking = {
+    enable = true;
+    hostName = "GARDEN";
+  };
+
+  myModules.ssh = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  myUsers.sana = {
+    enable = true;
+    useHomeManager = true;
+    sshAccessPermitted = true;
+    windowManager = "dwm";
+  };
+
+  networking.modemmanager.enable = true;
+  networking.firewall.enable = false;
+
+  system.stateVersion = "25.11";
+}

651f9ba79652fe3d00357484fea11bb0ade27e2f
doppelsana <sana@doppel.moe>
2026-02-10 09:51:34 -0500

    adding fcc unlock script for the wwan modem

--- hosts/GARDEN/default.nix
+++ hosts/GARDEN/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hardware.nix
   ];
@@ -46,7 +46,15 @@
     windowManager = "dwm";
   };
 
-  networking.modemmanager.enable = true;
+  networking.modemmanager = {
+    enable = true;
+    fccUnlockScripts = [
+      {
+        id = "1199:9079";
+        path = "${pkgs.modemmanager}/share/ModemManager/fcc-unlock.available.d/1199:9079";
+      }
+    ];
+  };
   networking.firewall.enable = false;
 
   system.stateVersion = "25.11";

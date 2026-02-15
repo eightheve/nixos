2ffde45b267d224fddcca1eaddef157c7e064d81
doppelsana <sana@doppel.moe>
2026-02-15 09:03:02 -0500

    using manual wireguard config instead of module

--- hosts/KAZOOIE/default.nix
+++ hosts/KAZOOIE/default.nix
@@ -10,7 +10,8 @@
 
   networking = {
     hostName = "KAZOOIE";
-    firewall.allowedUDPPorts = [51820 51821];
+    firewall.allowedTCPPorts = [443 80 2086];
+    firewall.allowedUDPPorts = [51820 51821 2086];
   };
 
   myUsers.sana = {
@@ -25,7 +26,7 @@
   };
 
   myModules.wireguard = {
-    enable = true;
+    enable = false;
     interfaces.wg0 = {
       role = "server";
       ip = "10.100.0.1/24";
@@ -49,6 +50,39 @@
     enable = true;
     openFirewall = true;
     ports = [2222];
+  };
+
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "10.100.0.1/24" ];
+    listenPort = 51820;
+    privateKeyFile = "/etc/wireguard/privatekey";
+    peers = [
+      {
+        publicKey = "0hrwVOfaPGTs2bfHoGrHroHGqG2aJiiu8JO9o5/K0xg=";
+        allowedIPs = [ "10.100.0.2/32" ];
+      }
+    ];
+  };
+
+  networking.enableIPv6 = true;
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = true;
+    "net.ipv6.conf.all.forwarding" = true;
+  };
+
+  networking.nat = {
+    enable = true;
+    externalInterface = "enp1s0";
+    internalInterfaces = [ "wg0" ];
+  };
+
+  networking.firewall = {
+    extraCommands = ''
+      iptables -t nat -F PREROUTING
+      iptables -t nat -A PREROUTING -i enp1s0 -p tcp ! --dport 2222 -j DNAT --to-destination 10.100.0.2
+      iptables -t nat -A PREROUTING -i enp1s0 -p udp ! --dport 51820 -j DNAT --to-destination 10.100.0.2
+      iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+    '';
   };
 
   system.stateVersion = "25.05";
